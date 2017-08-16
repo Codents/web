5e052ed19dd693058b2d2d0a0459775d17a78eb5
Manuel Alfaro Sierra <dokoto.moloko@gmail.com>
2017-08-16 11:08:46 +0200

    dev test

--- test/bdd/features/step_definitions/home_steps.js
+++ test/bdd/features/step_definitions/home_steps.js
@@ -2,6 +2,8 @@ const { By, until } = require('selenium-webdriver');
 const Assert = require('selenium-webdriver/testing/assert');
 const { defineSupportCode } = require('cucumber');
 
+const TIMEOUT = 10000;
+
 defineSupportCode(({ Given, When, Then }) => {
   Given(/^Accedo a la Home en ([^"]*)$/, function (url) {
     return this.driver.get(url);
@@ -9,8 +11,13 @@ defineSupportCode(({ Given, When, Then }) => {
 
   When(/^Hago click en el menu ([^"]*)$/, function (menu) {
     return this.driver
-      .wait(until.elementLocated(By.css(menu)), 10000)
-      .then(element => element.click());
+      .wait(until.elementLocated(By.css(menu)), TIMEOUT)
+      .then(function (element) {
+        element.click();
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
   });
 
   Then(/^Estoy en la seccion ([^"]*)$/, function (menu) {
@@ -19,25 +26,32 @@ defineSupportCode(({ Given, When, Then }) => {
 
   When(/^Hago click en el icono de ([^"]*)$/, function (icono) {
     return this.driver
-      .wait(until.elementLocated(By.css(icono)), 10000)
-      .then(element => element.click());
+      .wait(until.elementLocated(By.css(icono)), TIMEOUT)
+      .then(function (element) {
+        element.click();
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
   });
 
   Then(
     /^Estoy en pagina del proyecto en "([^"]*)" y cliqueo en el icono con clase ([^"]*)$/,
     function (githubUrl, icono) {
       if (icono === '.github-icon') {
-        return this.driver.wait(until.elementLocated(By.css('strong a')), 10000).then(element => {
-          element
-            .getAttribute('href')
-            .then(function (attr) {
-              const assert = new Assert(attr === githubUrl);
-              assert.isTrue('Fallo al navegar hasta el proyecto en Github');
-            })
-            .catch(function (err) {
-              console.error(err);
-            });
-        });
+        return this.driver
+          .wait(until.elementLocated(By.css('strong a')), TIMEOUT)
+          .then(function (element) {
+            element
+              .getAttribute('href')
+              .then(function (attr) {
+                const assert = new Assert(attr === githubUrl);
+                assert.isTrue('Fallo al navegar hasta el proyecto en Github');
+              })
+              .catch(function (err) {
+                console.error(err);
+              });
+          });
       }
       return false;
     }
